b5ff793b7d3fc0b6a7000645590c123d6e337786
lambda <fluss.der.trauer@gmail.com>
2016-08-09 00:51:06 +0300

    Register product models

--- src/apps/product/admin.py
+++ src/apps/product/admin.py
@@ -1,3 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import ProductModel, CommentModel
+
+
+@admin.register(ProductModel)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ('name', 'user', 'created')
+    ordering = ('created', 'name',)
+    list_filter = ('created',)
+
+
+@admin.register(CommentModel)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('comment_text', 'product', 'user',)
+    ordering = ('product', 'user',)
+    list_filter = ('product', 'user',)
+    search_fields = ('comment',)
+
+    @staticmethod
+    def comment_text(obj):
+        if len(obj.comment) > 70:
+            return '{0} ...'.format(obj.comment[:70])
+        return obj.comment
