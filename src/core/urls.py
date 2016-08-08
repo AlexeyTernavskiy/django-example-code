69dd53b413103fedd8b30a0934cf905cbe068487
lambda <fluss.der.trauer@gmail.com>
2016-08-09 01:32:31 +0300

    Update core urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from allauth.account import views
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
@@ -8,5 +9,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^signup/$', views.signup, name='account_signup'),
+    url(r'^login/$', views.login, name='account_login'),
+    url(r'^logout/$', views.logout, name='account_logout'),
     url(r'^products/', include('src.apps.product.urls', namespace='products'))
 ]
