a127b1b16338afce987db1f83dffbbe166e4712d
lambda <fluss.der.trauer@gmail.com>
2016-08-09 00:54:53 +0300

    Create product views

--- src/apps/product/views.py
+++ src/apps/product/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+
+from src.apps.product.models import ProductModel
+
+
+class ProductsListView(ListView):
+    model = ProductModel
+    template_name = 'pages/products.html'
+    context_object_name = 'products'
+    paginate_by = 5
+
+
+class ProductDetailView(DetailView):
+    model = ProductModel
+    template_name = 'pages/product.html'
+    context_object_name = 'product'
