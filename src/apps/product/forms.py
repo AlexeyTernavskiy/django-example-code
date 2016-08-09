7661c9c56fce10a2dd2ceada7cec85a05a11b4b7
lambda <fluss.der.trauer@gmail.com>
2016-08-09 04:59:40 +0300

    Add create product form

--- src/apps/product/forms.py
+++ src/apps/product/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from model_utils import Choices
 
+from src.apps.product.models import ProductModel
+
 
 class FilterForm(forms.Form):
     FILTER_CHOICE = Choices(
@@ -21,3 +23,34 @@ class FilterForm(forms.Form):
     )
 
 
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = ProductModel
+        exclude = ('user', 'slug',)
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Name',
+                    'required': True,
+                },
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'cols': 50,
+                    'row': 5,
+                    'placeholder': 'Description',
+                    'required': True,
+                },
+            ),
+            'price': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'type': 'number',
+                    'step': '0.01',
+                    'placeholder': 'Price',
+                    'required': True,
+                },
+            ),
+        }
