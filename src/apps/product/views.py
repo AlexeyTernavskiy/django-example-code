bb93245e8afefade445a5eb049d747bcc8b4718c
lambda <fluss.der.trauer@gmail.com>
2016-08-09 05:05:41 +0300

    Add create product view

--- src/apps/product/views.py
+++ src/apps/product/views.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import re
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import CreateView
 from django.views.generic import ListView, DetailView
 from pure_pagination import PaginationMixin
 
-from src.apps.product.forms import FilterForm
+from src.apps.product.forms import FilterForm, ProductForm
 from src.apps.product.models import ProductModel
 
 
@@ -37,3 +43,22 @@ class ProductDetailView(DetailView):
     queryset = ProductModel.objects.select_related('user').annotate(like_total=Count('like'))
     template_name = 'product/product.html'
     context_object_name = 'product'
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    form_class = ProductForm
+    template_name = 'product/form.html'
+
+    def get_success_url(self, **kwargs):
+        return reverse('products:detail', kwargs={
+            'slug': kwargs.get('slug')
+        })
+
+    def form_valid(self, form):
+        form = form.save(commit=False)
+        form.user = self.request.user
+        form.name = form.name.capitalize()
+        form.description = form.description.capitalize()
+        form.slug = re.sub("^\s+|\n|\r|\s+$", '', form.name.lower()).replace(' ', '-')
+        form.save()
+        return redirect(self.get_success_url(slug=form.slug))
